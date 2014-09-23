d82e37278201f3924aea1b6cd6319fd3b811f442
Nils Petersohn <post@berlin.de>
2014-09-23 14:58:54 +0200

    apply route

--- lib/devise_saml_authenticatable/routes.rb
+++ lib/devise_saml_authenticatable/routes.rb
@@ -3,7 +3,7 @@ ActionDispatch::Routing::Mapper.class_eval do
   def devise_saml_authenticatable(mapping, controllers)
     resource :session, :only => [], :controller => controllers[:saml_sessions], :path => "" do
       get :new, :path => "saml/sign_in", :as => "new_saml"
-      post :create, :path=>"saml/auth"
+      post :create, :path=>"saml/consume"
       delete :destroy, :path => mapping.path_names[:sign_out], :as => "destroy_saml", :via => mapping.sign_out_via
       get :metadata, :path=>"saml/metadata"
     end
