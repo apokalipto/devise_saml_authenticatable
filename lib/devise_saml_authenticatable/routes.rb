29fd6a994907b27f819dc24108cc92b729daf0d2
Nils Petersohn <post@berlin.de>
2014-09-23 13:59:05 +0200

    rename gem + solve path conflicts

--- lib/devise_saml_authenticatable/routes.rb
+++ lib/devise_saml_authenticatable/routes.rb
@@ -2,9 +2,9 @@ ActionDispatch::Routing::Mapper.class_eval do
   protected
   def devise_saml_authenticatable(mapping, controllers)
     resource :session, :only => [], :controller => controllers[:saml_sessions], :path => "" do
-      get :new, :path => "saml/sign_in", :as => "new"
+      get :new, :path => "saml/sign_in", :as => "new_saml"
       post :create, :path=>"saml/auth"
-      delete :destroy, :path => mapping.path_names[:sign_out], :as => "destroy", :via => mapping.sign_out_via
+      delete :destroy, :path => mapping.path_names[:sign_out], :as => "destroy_saml", :via => mapping.sign_out_via
       get :metadata, :path=>"saml/metadata"
     end
   end
