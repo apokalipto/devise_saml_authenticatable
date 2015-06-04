9e0dbbe21a854f893d8816a2f6bdb97c7ba26c42
Mitchel Lindsay <mitch.lindsay@gmail.com>
2015-06-04 15:30:59 -0700

    Initial setup of the spec framework.  In a known bad state but commiting early to share current state.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,11 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in devise_saml_authenticatable.gemspec
 gemspec
+
+group :test do
+  gem 'rspec', '~> 3.0'
+  gem 'activemodel'
+  gem 'rails'
+  gem 'rspec-rails'
+end
+
---
+++ spec/controllers/devise/saml_sessions_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'devise/modules'
+
+class Devise::SessionsController < ActionController::Base
+
+end
+
+require_relative '../../../app/controllers/devise/saml_sessions_controller'
+
+describe Devise::SamlSessionsController, type: :controller do
+
+  before do
+    @saml_config = OneLogin::RubySaml::Settings.new({})
+  end
+
+  describe '#new' do
+    it 'redirects to the SAML Auth Request endpoint' do
+      get :new
+      expect(response).to redirect_to(OneLogin::RubySaml::Authrequest.new.create(@saml_config))
+    end
+  end
+
+end
