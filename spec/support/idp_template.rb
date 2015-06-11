487ca1003cdeb5c33f678db7b05dc0cf2dc381ec
Adam Stegman <astegman@onemedical.com>
2015-06-11 15:57:42 -0700

    Use thor templating

--- spec/support/idp_template.rb
+++ spec/support/idp_template.rb
@@ -1,12 +1,10 @@
 # Set up a SAML IdP
 
-include_subject_in_attributes = ask("Include the subject in the attributes?", limit: %w(y n)) == "y"
+@include_subject_in_attributes = ask("Include the subject in the attributes?", limit: %w(y n)) == "y"
 
 gem 'ruby-saml-idp'
 
 route "get '/saml/auth' => 'saml_idp#new'"
 route "post '/saml/auth' => 'saml_idp#create'"
 
-saml_idp_controller_template = File.read(File.expand_path('../saml_idp_controller.rb.erb', __FILE__))
-saml_idp_controller_contents = ERB.new(saml_idp_controller_template).result(binding)
-create_file 'app/controllers/saml_idp_controller.rb', saml_idp_controller_contents
+template File.expand_path('../saml_idp_controller.rb.erb', __FILE__), 'app/controllers/saml_idp_controller.rb'
