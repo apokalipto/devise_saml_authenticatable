0b9b24fd634cf109c8d8d6d765c986e1feb8c3de
Adam Stegman <astegman@onemedical.com>
2016-07-07 08:21:22 -0700

    Extract bad destination into a method

--- spec/support/idp_template.rb
+++ spec/support/idp_template.rb
@@ -23,11 +23,7 @@ route "post '/saml/auth' => 'saml_idp#create'"
 route "get '/saml/logout' => 'saml_idp#logout'"
 route "get '/saml/sp_sign_out' => 'saml_idp#sp_sign_out'"
 
-if @valid_destination == "false"
-  template File.expand_path('../saml_idp_bad_destination_controller.rb.erb', __FILE__), 'app/controllers/saml_idp_controller.rb'
-else
-  template File.expand_path('../saml_idp_controller.rb.erb', __FILE__), 'app/controllers/saml_idp_controller.rb'
-end
+template File.expand_path('../saml_idp_controller.rb.erb', __FILE__), 'app/controllers/saml_idp_controller.rb'
 
 copy_file File.expand_path('../saml_idp-saml_slo_post.html.erb', __FILE__), 'app/views/saml_idp/saml_slo_post.html.erb'
 create_file 'public/stylesheets/application.css', ''
