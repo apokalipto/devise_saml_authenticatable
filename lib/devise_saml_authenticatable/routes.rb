2c03bc95e3f4d91d28421c3cb8ecb260f8198178
jotase <jota.segovia@gmail.com>
2017-08-07 16:03:41 -0400

    rename routes to avoid override others devise strategies routes

--- lib/devise_saml_authenticatable/routes.rb
+++ lib/devise_saml_authenticatable/routes.rb
@@ -1,12 +1,13 @@
 ActionDispatch::Routing::Mapper.class_eval do
   protected
+
   def devise_saml_authenticatable(mapping, controllers)
-    resource :session, :only => [], :controller => controllers[:saml_sessions], :path => "" do
-      get :new, :path => "saml/sign_in", :as => "new"
-      post :create, :path=>"saml/auth"
-      match :destroy, :path => mapping.path_names[:sign_out], :as => "destroy", :via => mapping.sign_out_via
-      get :metadata, :path=>"saml/metadata"
-      match :idp_sign_out, :path=>"saml/idp_sign_out", via: [:get, :post]
+    resource :session, only: [], controller: controllers[:saml_sessions], path: '' do
+      get :new, path: 'saml/sign_in', as: :new_user_saml_session
+      post :create, path: 'saml/auth', as: :user_saml_session
+      match :destroy, path: mapping.path_names[:sign_out], as: :destroy_user_saml_session, via: mapping.sign_out_via
+      get :metadata, path: 'saml/metadata', as: :metadata_user_saml_session
+      match :idp_sign_out, path: 'saml/idp_sign_out', via: %i[get post]
     end
   end
 end
