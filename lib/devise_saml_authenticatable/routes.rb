ce79186518692e51e7da9515d7490d01ef6d1b41
Adam Stegman <astegman@onemedical.com>
2019-01-10 15:35:41 -0600

    Fix Ruby 1.9

--- lib/devise_saml_authenticatable/routes.rb
+++ lib/devise_saml_authenticatable/routes.rb
@@ -8,7 +8,7 @@ ActionDispatch::Routing::Mapper.class_eval do
         post :create, path: 'saml/auth', as: prefix
         match :destroy, path: mapping.path_names[:sign_out], as: "destroy_#{prefix}", via: mapping.sign_out_via
         get :metadata, path: 'saml/metadata'
-        match :idp_sign_out, path: 'saml/idp_sign_out', as: "idp_destroy_#{prefix}", via: %i[get post]
+        match :idp_sign_out, path: 'saml/idp_sign_out', as: "idp_destroy_#{prefix}", via: [:get, :post]
       end
     else
       resource :session, only: [], controller: controllers[:saml_sessions], path: '' do
@@ -16,7 +16,7 @@ ActionDispatch::Routing::Mapper.class_eval do
         post :create, path: 'saml/auth'
         match :destroy, path: mapping.path_names[:sign_out], as: 'destroy', via: mapping.sign_out_via
         get :metadata, path: 'saml/metadata'
-        match :idp_sign_out, path: 'saml/idp_sign_out', via: %i[get post]
+        match :idp_sign_out, path: 'saml/idp_sign_out', via: [:get, :post]
       end
     end
   end
