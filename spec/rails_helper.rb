0ff11e63ac2b6d43f21a92e59c514689c661b131
Eric Willoughby <willouge@mskcc.org>
2025-04-09 09:08:24 -0400

    Handle Rails 8 now lazy loading routes impacting tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,19 @@ create_app('sp', 'USE_SUBJECT_TO_AUTHENTICATE' => "false")
 require "#{working_directory}/sp/config/environment"
 require 'rspec/rails'
 
+# Starting from Rails 8.0, routes are lazy-loaded by default in test and development environments.
+# However, Devise's mappings are built during the routes loading phase.
+# To ensure it works correctly, we need to load the routes first before accessing @@mappings.
+if defined?(Rails) && Gem::Version.new(Rails.version) > Gem::Version.new('7.2')
+  require 'devise'
+  module Devise
+    def self.mappings
+      Rails.application.try(:reload_routes_unless_loaded)
+      @@mappings
+    end
+  end
+end
+
 ActiveRecord::Migration.verbose = false
 ActiveRecord::Base.logger = Logger.new(nil)
 if ActiveRecord::Base.connection.respond_to?(:migration_context)
